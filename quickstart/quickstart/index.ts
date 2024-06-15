25043baa373d57694f938c047f174002a172030a
raymondsquared <bolesray@outlook.com>
2024-06-15 23:46:47 +1000

    feat: create bucket endpoint
    make s3-bucket-endpoint

--- quickstart/quickstart/index.ts
+++ quickstart/quickstart/index.ts
@@ -3,13 +3,40 @@ import * as aws from '@pulumi/aws';
 import * as awsx from '@pulumi/awsx';
 
 // Create an AWS resource (S3 Bucket)
-const bucket = new aws.s3.Bucket('my-bucket');
-
-// Create an S3 Bucket object
-const bucketObject = new aws.s3.BucketObject('index.html', {
-  bucket: bucket.id,
-  source: new pulumi.asset.FileAsset('./index.html'),
+const bucket = new aws.s3.Bucket('my-bucket', {
+  website: {
+    indexDocument: 'index.html',
+  },
 });
+const ownershipControls = new aws.s3.BucketOwnershipControls(
+  'ownership-controls',
+  {
+    bucket: bucket.id,
+    rule: {
+      objectOwnership: 'ObjectWriter',
+    },
+  },
+);
+
+const publicAccessBlock = new aws.s3.BucketPublicAccessBlock(
+  'public-access-block',
+  {
+    bucket: bucket.id,
+    blockPublicAcls: false,
+  },
+);
+
+const bucketObject = new aws.s3.BucketObject(
+  'index.html',
+  {
+    bucket: bucket.id,
+    source: new pulumi.asset.FileAsset('./index.html'),
+    contentType: 'text/html',
+    acl: 'public-read',
+  },
+  { dependsOn: [publicAccessBlock, ownershipControls] },
+);
 
 // Export the name of the bucket
 export const bucketName = bucket.id;
+export const bucketEndpoint = pulumi.interpolate`http://${bucket.websiteEndpoint}`;
