af904e8314fbabe279e94db3e862416294836ebc
raymondsquared <bolesray@outlook.com>
2024-06-15 23:28:42 +1000

    feat: deploy file to s3
    make s3-ls

--- quickstart/quickstart/index.ts
+++ quickstart/quickstart/index.ts
@@ -1,9 +1,15 @@
-import * as pulumi from "@pulumi/pulumi";
-import * as aws from "@pulumi/aws";
-import * as awsx from "@pulumi/awsx";
+import * as pulumi from '@pulumi/pulumi';
+import * as aws from '@pulumi/aws';
+import * as awsx from '@pulumi/awsx';
 
 // Create an AWS resource (S3 Bucket)
-const bucket = new aws.s3.Bucket("my-bucket");
+const bucket = new aws.s3.Bucket('my-bucket');
+
+// Create an S3 Bucket object
+const bucketObject = new aws.s3.BucketObject('index.html', {
+  bucket: bucket.id,
+  source: new pulumi.asset.FileAsset('./index.html'),
+});
 
 // Export the name of the bucket
 export const bucketName = bucket.id;
